b088a435ddbd8219b24913fe89335468807439aa
Andrés Carreño <andresdavid90@gmail.com>
2016-03-31 19:59:44 -0500

    First commit

---
+++ src/index.test.js
@@ -0,0 +1,12 @@
+var expect = require('expect');
+var pizzaGuy = require('./index.js');
+
+describe('pizza-guy', function(){
+
+  describe('something', function(){
+    it('and test something', function(){
+      expect('something truthy').toExist();
+    });
+  });
+
+});
