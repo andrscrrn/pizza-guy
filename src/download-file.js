82723e7254240d6111633f2210f1cb91e2b48b91
baldore <acornssn@gmail.com>
2016-08-09 12:12:58 -0500

    fix(mock-server): Add some fixes based on the PR comments
    Add the missing dependency nock and change the JSDoc of download-file to use String

--- src/download-file.js
+++ src/download-file.js
@@ -4,9 +4,9 @@ import request from 'request';
 /**
  * Establish TCP connection for saving an image on disk
  * @param  {Object} file object containing data for the download process
- * @param  {string} file.fileName name of the file
- * @param  {string} file.host host for the request
- * @param  {string} file.path path for the request.
+ * @param  {String} file.fileName name of the file
+ * @param  {String} file.host host for the request
+ * @param  {String} file.path path for the request.
  * @return {Promise}
  */
 const downloadFile = (file) => {
@@ -21,13 +21,9 @@ const downloadFile = (file) => {
       if (err) { // File doesn't exists yet
         request
           .get(`http://${host}${path}`)
-          .on('error', (error) => {
-            reject(error, { fileName });
-          })
+          .on('error', (error) => reject(error, { fileName }))
           .pipe(fs.createWriteStream(fileName))
-          .on('close', () => {
-            resolve({ fileName, isRepeated: false });
-          });
+          .on('close', () => resolve({ fileName, isRepeated: false }));
       } else { // File is already on disk
         resolve({ fileName, isRepeated: true });
       }
